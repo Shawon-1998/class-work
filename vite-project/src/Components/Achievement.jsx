57ae87d2002d2d5dd110efd61d3ccabb678e388d
shawon-1998 <billalhossainshawon989@gmail.com>
2026-02-18 00:00:06 +0600

    added

---
+++ vite-project/src/Components/Achievement.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Container from './Container'
+
+const Achievement = () => {
+  return (
+    <>
+      <Container>
+          <h4 className='text-center'>Behind the Pixels</h4>
+          <h1 className='text-[47px] font-semibold mt-6.5 mb-14.75'>MY Background and <span className='block'>Achievements</span></h1>
+      </Container>
+    </>
+  )
+}
+
+export default Achievement
